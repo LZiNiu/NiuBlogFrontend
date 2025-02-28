c63ea0ac49b0f22a5c158cf5e1e7a76ab396485c
wordnew <2890823938@qq.com>
2025-02-28 22:14:09 +0800

    upgrade:添加了切换路由时的页面过渡动画,增加文字的打字机效果

--- src/components/Layout/main/index.vue
+++ src/components/Layout/main/index.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="main-container">
+        <!-- Page Header -->
         <page-header></page-header>
-        <router-view></router-view>
+        <!-- Main Content -->
+        <div class="main-content-wrapper">
+            <router-view v-slot="{ Component }">
+                <transition name="content-fade" mode="out-in">
+                    <component :is="Component" />
+                </transition>
+            </router-view>
+        </div>
+
     </div>
 </template>
 
@@ -10,6 +19,26 @@ import PageHeader from '@/components/PageHeader/index.vue';
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+.main-content-wrapper {
+    width: 100%;
+    height: 100%;
+    background-image: url('@/assets/images/home_bg.jpg');
+    background-position: center;
+}
+
+/* #region添加路由切换进入动画 */
+.content-fade-enter-active {
+    transition: opacity 0.5s ease, transform 0.5s ease;
+}
+.content-fade-enter-from  {
+    opacity: 0;
+    transform: translateY(20px);
+}
+.content-fade-enter-to {
+    opacity: 1;
+    transform: translateY(0px);
+}
+/* #endregion */
 </style>
--- src/components/PageHeader/index.vue
+++ src/components/PageHeader/index.vue
@@ -1,43 +1,76 @@
 <template>
-    <div id="page-header" class="page-header">
-        <div id="home-banner" class="home-banner" v-if="$route.path == '/home'">
-            
-            <div class="banner-content">
-                <span class="banner-title text-3xl">
-                    Cattle Blog
-                </span>
-                <span class="banner-saying text-xl">
-                    Hello World!!!
-                </span>
+    <transition name="header-fade" mode="out-in">
+        <div id="page-header" class="page-header" v-if="shouldAnimate" :style="{ backgroundImage: `url(${page_bg})` }">
+            <div id="home-banner" class="home-banner" v-if="$route.path == '/home'">
+                
+                <div class="banner-content">
+                    <span class="banner-title text-3xl">
+                        Cattle Blog
+                    </span>
+                    <span class="banner-saying text-xl">
+                        {{ currentText }}
+                    </span>
+                </div>
+                <div class="scroll-down" @click="scrollToHomeContent">
+                    <CiChevronDown class="scroll-arrow-icon"></CiChevronDown>
+                </div>
             </div>
-            <div class="scroll-down" @click="scrollToHomeContent">
-                <CiChevronDown class="scroll-arrow-icon"></CiChevronDown>
+            <div class="article-banner z-[2]" v-else-if="$route.path == '/article'">
+                <div class="banner-content">
+                    <span class="banner-title">
+                        {{ $route.query.articleId }}
+                    </span>
+                </div>
             </div>
-        </div>
-        <div class="article-banner z-[2]" v-else-if="$route.path == '/article'">
-            <div class="banner-content">
-                <span class="banner-title">
-                    {{ $route.query.articleId }}
-                </span>
-            </div>
-        </div>
 
-        <div class="other z-[2]" v-else>
-            <div class="banner-content">
-                <span class="banner-title">
-                    {{ $route.name }}
-                </span>
+            <div class="other z-[2]" v-else>
+                <div class="banner-content">
+                    <span class="banner-title">
+                        {{ $route.name }}
+                    </span>
+                </div>
             </div>
         </div>
-    </div>
+    </transition>
     
 </template>
 
 <script setup lang="ts">
 import CiChevronDown from '~icons/ci/chevron-down';
 
+const route = useRoute();
+// 背景图路径
+let page_bg = ref(`'src/assets/images/${route.path}_banner.jpg'`);
 
-// let showScrollDown = ref(true);
+const shouldAnimate = ref(true);
+
+watch(() => route.path, (newPath) => {
+
+    // 更新背景图
+    page_bg.value = `'src/assets/images/${route.path}_banner.jpg'`;
+
+    // 方案一: v-show同时触发过渡效果和文字动画,同时避免v-if重新渲染
+    // // 隐藏组件以触发动画
+    // shouldAnimate.value = false;
+    // // 延迟后显示组件,同时触发文字动画以及pageheader过渡
+    // setTimeout(() => {
+    //     shouldAnimate.value = true;
+    // }, 10);
+
+    // 方案二: 当使用v-if时才生效,若使用v-show可能会由于rAF执行时间过短造成动画无效,本身pageheader组件不复杂,重新渲染开销可以接受
+    // 方案一在频繁切换时仅display的值迅速发生变化,但是可能由于DOM元素来不及重新渲染,动画可能无效,出现闪屏现象,故最终采用方案二
+    shouldAnimate.value = false;
+
+    // 使用 rAF 确保动画与 DOM 更新同步
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            shouldAnimate.value = true; // 显示组件
+        });
+    });
+
+});
+
+
 function scrollToHomeContent() {
     // 获取 page-header 元素
     const PageHeader = document.getElementById('page-header');
@@ -53,15 +86,75 @@ function scrollToHomeContent() {
     });
 }
 
+const sayings = ['Hello World!!!', 'Welcome to Cattle Blog']; // 循环显示的文本
+const currentIndex = ref(0); // 当前显示的文本索引
+const currentText = ref(''); // 当前显示的文本
+const isDeleting = ref(false); // 是否处于删除状态
+let typingInterval:any = null; // 定时器引用
+
+// 打字机效果逻辑
+const typeWriter = () => {
+    const fullText = sayings[currentIndex.value]; // 获取当前完整文本
+    const textLength = isDeleting.value ? currentText.value.length - 1 : currentText.value.length + 1;
+
+    // 更新当前文本
+    currentText.value = isDeleting.value
+        ? fullText.substring(0, textLength)
+        : fullText.substring(0, textLength);
+
+    // 判断是否完成打字或删除
+    if (!isDeleting.value && currentText.value === fullText) {
+        setTimeout(() => {
+            isDeleting.value = true; // 开始删除
+        }, 1000); // 停留 1 秒后开始删除
+    } else if (isDeleting.value && currentText.value === '') {
+        isDeleting.value = false; // 停止删除
+        currentIndex.value = (currentIndex.value + 1) % sayings.length; // 切换到下一个文本
+    }
+};
+
+onMounted(() => {
+    if (route.path === '/home')
+        typingInterval = setInterval(typeWriter, 300); // 每 100ms 更新一次
+});
+
+onUnmounted(()=>{
+    if (typingInterval) {
+        clearInterval(typingInterval);
+    }
+})
+
+watch(()=>route.path, () =>{
+    if (route.path === '/home' || route.path === '/')
+        typingInterval = setInterval(typeWriter, 300); // 每 100ms 更新一次
+    else{
+        if (typingInterval) {
+            clearInterval(typingInterval);
+            currentText.value = '';
+            console.log('定时器已清理');
+        }
+    }
+})
+
+
 </script>
 
 <style lang="scss" scoped>
+
+.header-fade-enter-active {
+    transition: transform 0.5s ease;
+}
+.header-fade-enter-from  {
+    transform: translateY(-20px);
+}
+.header-fade-enter-to {
+    transform: translateY(0px);
+}
 
 .page-header {
     width: 100%;
     overflow: hidden;
     position: relative;
-    background-image: url('@/assets/images/home_banner.jpg');
     // background-attachment: fixed;
     background-size: cover;
     background-position: center;
@@ -122,6 +215,8 @@ function scrollToHomeContent() {
     }
 }
 
+
+/* #region页面头部图片中的文字样式 */
 .banner-content {
     position: absolute;
     left: 50%;
@@ -132,14 +227,44 @@ function scrollToHomeContent() {
         font-weight: bold;
         display: block;
         text-align: center;
+        animation: text_scale 1s;
     }
     .banner-saying {
-        display: block;
+        display: inline-block;
         text-align: center;
+        overflow: hidden;
         
+
+        &::after {
+            content: '';
+            margin-left: 2px;
+            border-left: 3px solid white;
+            animation: blink_caret 0.75s infinite;
+        }
     }
 }
 
+/* 光标闪烁动画 */
+@keyframes blink_caret {
+    from, to {
+    border-color: transparent; /* 光标隐藏 */
+    }
+    50% {
+    border-color: white; /* 光标显示 */
+    }
+}
+
+@keyframes text_scale {
+    0% {
+        opacity: 0;
+        transform: scale(0.7);
+    }
+    100% {
+        opacity: 1;
+        transform: scale(1);
+    }
+}
+/*#endregion */
 
 .article-banner,
 .other {
