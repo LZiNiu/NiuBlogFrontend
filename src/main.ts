b3034f3633424dbba67706c7d88f8cde6fb0ef08
wordnew <2890823938@qq.com>
2025-11-30 13:54:56 +0800

    fix: 修正引入顺序引发的样式失效问题

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
-//引入重置样式
-import './styles/reset.scss'
-//引入全局样式
-import "./styles/global.scss";
-//引入element-plus自定义样式
-import './styles/element/custom.scss'
 //引入tailwindcss
 import './styles/tailwind.css';
+//引入element-plus自定义样式
+import './styles/element/custom.scss'
+
 
 //引入字体图标
 import './assets/icons/iconfont.css'
